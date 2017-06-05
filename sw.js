38caafddbb128c4b2a21429b5af5902bc5279e20
iain plimmer <iainplimmer@iains-iMac.local>
2017-06-05 22:21:43 +0100

    We now add pages to our cahe as they are visited.

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const version = 'v1';
+const version = 'v2.3';
 let log = console.log.bind(console);
 
 //  Event automatically called on registration of the service worker by 'navigator.serviceWorker.register'
 self.addEventListener('install', function (event){
-   
     log(`${version} installed at ${ new Date().toLocaleTimeString()}`);
    
+   //   Creates the cache of offline files that we can defer to later when we lose connectivity.
     event.waitUntil(
         caches.open(version)
-        .then(function(cache) {
+        .then(cache => {
             return cache.addAll([
                 './offline/index.html',
                 './offline/testing.js'
@@ -19,9 +19,23 @@ self.addEventListener('install', function (event){
 //  Event automatically called on activation of the service worker by 'skipWaiting'
 self.addEventListener('activate', function (event){
    log(`${version} activated at ${ new Date().toLocaleTimeString()}`);
+
+   //   We can clean up any caches that do not match our version here
+   event.waitUntil(
+        caches.keys()
+        .then(keys => {
+            return Promise.all(keys.filter(function (key) {
+                return key !== version;
+            })
+            .map(key => {
+                log(`Cleaning up the cache key ${key}`);
+                return caches.delete(key);
+            }));
+        }));
 });
 
 
+//  Event called that intercepts and responds to a classic fetch from the browser to the server.
 self.addEventListener('fetch', function (event) {
 
     //  IMPORTANT NOTE!
@@ -40,21 +54,43 @@ self.addEventListener('fetch', function (event) {
     //     event.respondWith(fetch(event.request));
     // }
 
+    //  We intercept and respond to the fetch from the browser here
     event.respondWith(
         caches.match(event.request)
-        .then(function (res) {
+        .then(res => {
+            
+            //  Is there a response from the cache? If there is return it.
             if (res) {
+                log(`${res.url} is actually coming from the cache now....`)
                 return res;
             }
 
+            //  Are we offline? If we are then let's return the offline version of our site
             if (!navigator.onLine) {
                 return caches.match(new Request('offline/index.html'));
             }
 
-            return fetch(event.request);
+            //  Otherwise we're online, so let's get our content from the server
+            return _fetchAndUpdate(event.request);
         })
 
     );
+});
 
-
-})
+//  The purpose of this function is to automatically take any reponse and put it into the
+//  service worker cache, this means that any part of our site that has been navigated to
+//  online will be accessible if the user is suddenly taken offline.
+function _fetchAndUpdate(request) {
+    return fetch (request)    
+    .then(res => {
+        if (res) {
+            return caches.open(version)
+            .then(cache => {
+                return cache.put(request, res.clone())
+                .then(() => {
+                    return res;
+                })
+            })
+        }
+    });
+}
