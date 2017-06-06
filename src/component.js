440a716acb3ab91d866483890e6bccb21a37605c
iain plimmer <iainplimmer@iains-iMac.local>
2017-06-06 21:59:38 +0100

    Data is now coming from a cache or the service!

--- src/component.js
+++ src/component.js
@@ -1,36 +1,22 @@
-(function (navigator) {
+(function () {
     angular.module('swApp')
         .component('datacomponent', {
             template: `
             <h3>There's Some Lovely Data Here!</h3>
-            <p ng-repeat="dataItem in $ctrl.MyData">
-                {{dataItem.title}}
-            </p>            
-            <p ng-if='$ctrl.FromCache'>
-                We can't connect to the internet, so there's no data!
-            </p>
+            <p ng-repeat="item in $ctrl.MyData">
+                {{ item.title }}
+            </p>              
             `,
             controllerAs: '$ctrl',
             controller: function(DataService) {
                 
                 let vm = this;
                 vm.MyData = [];
-                vm.FromCache = true;
-
-                if (!navigator.onLine) {
-                    console.log('im offline!')
-                    vm.FromCache = true;
-                }
-
-                DataService
-                .then(response => {
-                    vm.MyData = response.data.slice(0,5)
-                    vm.FromCache = false;
-                    console.log('helo?', vm.FromCache)
-                })
-                .catch(error => {
-                    vm.FromCache = true;
-                })
+              
+                DataService.GetData()
+                    .then(response => {                    
+                        vm.MyData = response;
+                    });
             }
         });
-})(navigator);
+})();
--- src/services.js
+++ src/services.js
@@ -1,6 +1,33 @@
-(function () {
+(function (navigator) {
     angular.module('swApp')
-    .service('DataService', function ($http) {
-        return $http.get('https://jsonplaceholder.typicode.com/posts');
-    })
-})();
+    .service('DataService', function ($http, $q) {
+        return {
+            GetData : function () {
+                if (!navigator.onLine) {
+                    //  We are offline, so let's fetch the data from the localStorage cache.
+                    return $q((resolve, reject) => {
+                        if (localStorage.getItem('application-data') === null) {
+                            console.error('No data found.');  
+                            reject(null);
+                        }
+                        else {
+                            console.info('Fetching data from the cache.');
+                            resolve(JSON.parse(localStorage.getItem('application-data'))); 
+                        }
+                    });
+                }
+                else {
+                    //  If we are online, let's get some data, then cache it in the localstorage for
+                    //  when we are offline later.
+                    return $http.get('https://jsonplaceholder.typicode.com/posts')
+                        .then(response => {           
+                            console.info('Fetching data from the service.');         
+                            let data = response.data.slice(0,5);
+                            localStorage.setItem('application-data', JSON.stringify(data));               
+                            return data;
+                        });
+                }
+            }
+        }
+    });
+})(navigator);
